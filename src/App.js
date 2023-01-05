19e7ae8f46325e7ea1c960c3f76557b525b87007
Jason Ross <jasonorlandoross@gmail.com>
2023-01-04 19:39:27 -0600

    nav bar functionality complete

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Navbar from './components/Navbar';
+import {BrowserRouter as Router, Route}  from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
+  }
+
+  export default App;
